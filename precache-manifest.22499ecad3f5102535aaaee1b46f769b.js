9c0ad5698f98fef4cc6fcaff010c4f6b8e708a0d
parjolandrei <parjol.andrei@yahoo.com>
2020-03-30 21:20:58 +0300

    Updates

---
+++ precache-manifest.22499ecad3f5102535aaaee1b46f769b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538b7bcfd27f91fa7a5f4e3597587af1",
+    "url": "/lyric-finder/index.html"
+  },
+  {
+    "revision": "519440d05e7c498ca4d4",
+    "url": "/lyric-finder/static/js/2.640353b4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/lyric-finder/static/js/2.640353b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0873ba952c545bdb896c",
+    "url": "/lyric-finder/static/js/main.3f58c358.chunk.js"
+  },
+  {
+    "revision": "3d869acbac012a290809",
+    "url": "/lyric-finder/static/js/runtime-main.cb7d720f.js"
+  }
+]);
